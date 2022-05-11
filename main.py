b201a613b7c0426e3ee2cca5a5eb26753df498c8
RitziaArkenstein <79651103+RitziaArkenstein@users.noreply.github.com>
2022-05-11 23:03:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -21,15 +21,7 @@ players = {}
 
 status = cycle(['Patting Ruina', 'Ruina Simulator'])
 
-chegg = ["chegg" , "cheggs", "Cheggs", "cheg", "Cheg"]
-bart = ["bartleby", "Bartleby", "bartlebys", "Barlebys"]
-solutioninn = ["solutioninn", "solutionin", "Solutioninn", "Solutioninn", "solution inn","Solution inn", "Solution in", "solution in"]
-scribd = ["scribd", "Scribd"]
-numerade = ["numerade", "Numerade"]
-hero = ["Coursehero", "coursehero", "Course hero","Course Hero","course hero", "course Hero"]
-slideshare = ["Slideshare", "slideshare", "Slide share", "Slide Share", "slide share", "slide Share", "slideshares", "slide shares", "Slide shares", "Slide Shares", "slide Shares"]
-tutor = ["transtutor", "Transtutor", "transtutors", "Transtutors", "Trans tutor", "Trans Tutor", "Trans tutors", "Trans Tutors", "trans Tutor", "trans Tutors"]
-wolfram = ["wolfram", "Wolfram", "wolframs", "Wolframs", "Wolf Ram", "Wolf ram", "Wolf rams", "Wolf Rams", "wolf ram", "wolf Rams", "wolf Ram"]
+bad = ["fuck" , "shit", "asshole", "bitch", "whore"]
 
 #no no words
 @client.event
@@ -42,56 +34,11 @@ async def on_message(message):
     print(message.content)
     if message.guild is not None:
         
-        for word in chegg:
+        for word in bad:
             # print(word)
             if message.content.lower().find(word)!=-1:
                 await message.delete()
-                await message.author.send("If you want help for Chegg solutions please go to the #manual-unlock  or #auto-unlock channel and wait for the admin/helpers there to answer your questions :)\n\n If you dont see the #manual-unlock/#auto-unlock channel or its locked, that means the admin/helper for unlocking ur answer is not available so please wait until they are available :)\n\n Please do read the #faq for more info Thanks")
-
-                # await client.process_commands(message)
-
-        for word in bart:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Bartlebys solutions please go to the #bartleby channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in solutioninn:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Solutioninn solutions please go to the #solutioninn channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in scribd:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Scribd solutions please go to the #scribd channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in numerade:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Numerade solutions please go to the #numerade channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in hero:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Coursehero solutions please go to the #coursehero channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in slideshare:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Slideshare solutions please go to the #slideshare channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in tutor:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Transtutor solutions please go to the #transtutor channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-        for word in wolfram:
-            if message.content.lower().find(word)!=-1:
-                await message.delete()
-                await message.author.send("If you want help for Wolfram solutions please go to the #wolfram channel and wait for the bot to answer your questions :)\n\n If the bot doesnt work as intended please DM one of the admins and they will help you if they are available :)")
-
-
-
+                await message.author.send("Hey! Please restrain from using that word from now on ! \n\n Try to be a better person by trying not to be rude ok?")
 
 #Tasks
 @tasks.loop(hours = 12)
